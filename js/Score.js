1a75b9da0ee7cafac02346f15aac82fa97880df6
RyunosukeSakurada <longzhizhu513@gmail.com>
2023-05-03 22:58:01 -0700

    complete

---
+++ js/Score.js
@@ -0,0 +1,94 @@
+// need to assign score dynamically
+const score = localStorage.getItem('score')
+const amount = localStorage.getItem('amount')
+function showEverything() {
+  document.querySelector('#score span + span').innerHTML = amount
+  setTimeout(() => {
+    makeChart()
+    countScore()
+    showCongratsText(score)
+  }, 1000)
+}
+window.addEventListener('DOMContentLoaded', showEverything)
+
+function makeChart() {
+  const ctx = document.getElementById('myChart')
+  const config = {
+    type: 'doughnut',
+    data: {
+      datasets: [
+        {
+          data: [score, amount - score],
+          backgroundColor: ['#28AFB0', 'transparent'],
+          borderWidth: 0,
+          hoverOffset: 0,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        tooltip: {
+          enabled: false,
+        },
+      },
+    },
+  }
+
+  new Chart(ctx, config)
+}
+
+function countScore() {
+  let count = 0
+  setInterval(function () {
+    if (count <= score) {
+      showScore(count)
+      count += 1
+    }
+  }, 30)
+}
+function showScore(count) {
+  const scoreEl = document.querySelector('#score span')
+  scoreEl.textContent = count
+}
+
+function showCongratsText(score) {
+  const congratsEl = document.getElementById('congrats')
+  const CONGRATS_TEXTS = [
+    'Perfect🥳🎉',
+    'Great Job👍',
+    'Good Job😁',
+    'Not Bad😏',
+    'Not Good😱',
+  ]
+
+  console.log((score / amount) * 100)
+
+  if (score == amount) {
+    congratsEl.textContent = CONGRATS_TEXTS[0]
+  } else if ((score / amount) * 100 >= 75) {
+    congratsEl.textContent = CONGRATS_TEXTS[1]
+  } else if ((score / amount) * 100 >= 50) {
+    congratsEl.textContent = CONGRATS_TEXTS[2]
+  } else if ((score / amount) * 100 >= 20) {
+    congratsEl.textContent = CONGRATS_TEXTS[3]
+  } else {
+    congratsEl.textContent = CONGRATS_TEXTS[4]
+  }
+}
+
+
+const tl = gsap.timeline({
+  defaults: {
+    alpha: 0,
+    duration: 0.8,
+    ease: 'InOut',
+  },
+})
+
+tl.from('.score-content', {
+  scale: 0,
+}).from('.congrats', {
+  delay: 1.2,
+  y: 10,
+})
---
+++ js/Trivia.js
@@ -0,0 +1,162 @@
+let score = 0
+const answerData = []
+
+window.addEventListener('DOMContentLoaded', showData)
+
+async function fetchQuizData(id, amount) {
+  const options = {
+    method: 'GET',
+    url: `https://opentdb.com/api.php?amount=${amount}&category=${id}&type=multiple`,
+  }
+  return await axios
+    .request(options)
+    .then(async function (response) {
+      const { results } = await response.data
+      return results
+    })
+    .catch(function (error) {
+      console.error(error)
+    })
+}
+
+
+async function showData(e) {
+  const category = localStorage.getItem('genre')
+  const id = localStorage.getItem('id')
+  const amount = localStorage.getItem('amount')
+  let trivia = await fetchQuizData(id, amount)
+  let [questions, answers] = getQuestions(trivia)
+
+  showQuestions(questions, answers, id, category, amount)
+}
+
+
+function getQuestions(trivia) {
+  let questions = []
+  let answers = []
+  for (const key in trivia) {
+    if (Object.hasOwnProperty.call(trivia, key)) {
+      const element = trivia[key]
+      questions.push(element.question)
+      answers.push(element.correct_answer)
+      let all_answers = element.incorrect_answers
+      all_answers.push(element.correct_answer)
+      answers.push(all_answers)
+    }
+  }
+
+  return [questions, answers]
+}
+
+
+function showQuestions(questions, answers, id, category, amount) {
+  let actual_question = 0
+  let isCorrect = false
+
+  // First question setup.
+  document.getElementById('category-title').innerHTML = category + ' Trivia '
+  updateData(questions, answers, actual_question)
+
+  // Button event listeners for prev and next functionality.
+  next_btn = document.getElementById('next-btn')
+  prev_btn = document.getElementById('previous-btn')
+
+  next_btn.addEventListener('click', function () {
+    let hasAnswered = checkAnswer(answers[2 * actual_question], isCorrect)
+    if (hasAnswered && score <= Number(amount)) {
+      actual_question < questions.length - 1
+        ? (actual_question += 1)
+        : (window.location.href = '../score/Score.html')
+
+      updateData(questions, answers, actual_question)
+    } else {
+      console.log('Answer!')
+    }
+  })
+  prev_btn.addEventListener('click', function () {
+    actual_question > 0 ? (actual_question -= 1) : (actual_question = 0)
+    updateData(questions, answers, actual_question)
+    checkAnswerData(answerData)
+  })
+}
+
+
+
+/* Updates question and answers html elements text */
+function updateData(questions, answers, actual_question) {
+  document.getElementById('remaining-question').innerHTML = `(${actual_question + 1} of ${questions.length})`
+  document.getElementById('question').innerHTML = questions[actual_question]
+
+  const correct_answer = answers[2 * actual_question]
+
+  let all_answers = answers[2 * actual_question + 1]
+  all_answers = randomShuffle(all_answers)
+  document.getElementById('answer1').innerHTML = all_answers[0]
+  document.getElementById('answer2').innerHTML = all_answers[1]
+  document.getElementById('answer3').innerHTML = all_answers[2]
+  document.getElementById('answer4').innerHTML = all_answers[3]
+
+  document.getElementById('answer1').previousElementSibling.value = all_answers[0]
+  document.getElementById('answer2').previousElementSibling.value = all_answers[1]
+  document.getElementById('answer3').previousElementSibling.value = all_answers[2]
+  document.getElementById('answer4').previousElementSibling.value = all_answers[3]
+
+  console.log({
+    correct_answer,
+  })
+}
+
+function checkAnswer(correct_answer, isCorrect){
+  let user_answer
+  let checked = false
+  document.querySelectorAll('.radio input').forEach(function (answer) {
+    if (answer.checked) {
+      user_answer = htmlDecode(answer.value)
+      countUpScore(htmlDecode(correct_answer), user_answer, isCorrect)
+      answer.checked = false
+      checked = true
+    }
+  })
+  return checked
+}
+
+//count up the score
+function countUpScore(correct_answer, user_answer, isCorrect) {
+  if (correct_answer === user_answer) {
+    score += 1
+    isCorrect = true
+  }
+  answerData.push(isCorrect)
+  localStorage.setItem('score', score)
+}
+
+
+function checkAnswerData(answerData) {
+  const prevAnswer = answerData.pop()
+  if (prevAnswer && score >= 0) {
+    score -= 1
+    localStorage.setItem('score', score)
+  }
+  console.log(score)
+}
+
+
+// decode escape sequences
+function htmlDecode(input) {
+  let doc = new DOMParser().parseFromString(input, 'text/html')
+  return doc.documentElement.textContent
+}
+
+function randomShuffle(array) {
+  let currentIndex = array.length,randomIndex
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    ;[array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ]
+  }
+  return array
+}
